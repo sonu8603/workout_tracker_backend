bbcacddde06a26090d80eb0549ae78edcf7dd592
Sonu kushwaha <sonukushwaha.9931@gmail.com>
2026-02-15 16:22:07 +0530

    chang SMTP port to 465 and secure true

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -5,7 +5,7 @@ const sendEmail = async (options) => {
     const transporter = nodemailer.createTransport({
       host: process.env.EMAIL_HOST, 
       port: Number(process.env.EMAIL_PORT), 
-      secure: false, 
+      secure: true, 
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
