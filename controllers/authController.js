09a4b33b9227f49655b77ad0295fc79f5471a240
Sonu kushwaha <sonukushwaha.9931@gmail.com>
2026-02-15 14:35:05 +0530

    Fix case sensitivity issue for User model

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,4 @@
-const User = require('../models/User');
+const User = require('../models/user');
 const jwt = require('jsonwebtoken');
 const crypto = require('crypto');
 const sendEmail = require('../utils/sendEmail');
